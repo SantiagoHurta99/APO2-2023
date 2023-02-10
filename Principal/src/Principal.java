0122d8ddc3e748967d341e53b1ef473e67fbe306
Estudiante USC <estudiante@GTACAD.USC>
2023-02-10 16:28:49 -0500

    primer cambio

--- Principal/src/Principal.java
+++ Principal/src/Principal.java
@@ -6,11 +6,15 @@ public class Principal {
 	// metodo principal
 	public static void main(String[] args) {
 
+		System.out.println(contarPalabras());
+	}
+	
+	public static String contarPalabras() {
 		System.out.println("Ingrese la palabra");
 		// clase que captura datos por consola
 		Scanner sc = new Scanner(System.in);
 		String prueba = sc.next();
-		//System.out.println("Hola mundo");
-		System.out.println("La palabra ("+prueba.toUpperCase()+") tiene "+prueba.length()+" caracteres");
+		
+		return "La palabra ("+prueba.toUpperCase()+") tiene "+prueba.length()+" caracteres";
 	}
 }
